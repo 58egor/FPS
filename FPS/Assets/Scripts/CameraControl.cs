44f8815c751a9e4bced2e48ebcaf62c9fb648622
58egor <68480726+58egor@users.noreply.github.com>
2021-03-15 21:28:44 +0300

    Was added spread system

---
+++ FPS/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Transform point;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = point.position;
+        transform.rotation = point.rotation;
+    }
+}
--- FPS/Assets/Scripts/RayShooting.cs
+++ FPS/Assets/Scripts/RayShooting.cs
@@ -10,6 +10,14 @@ public class RayShooting : MonoBehaviour
     public int maxAmmo=30;
     private bool isReload = false;
     public float reloadTimer;
+    public float maxRazbros;//максимальное значение рандома по коорлинате 
+    public float minRazbros;//минимальное значение рандома по коорлинате
+    private float thisRazbros;//текущее значение разброса
+    public float step;//шаг увелиивающий диапзоно выстрела
+    public int razbros;//количество выстрелов увеличивающий разброс
+    private int curRazbros;//текущий разброс
+    public float razbrosTimer;//таймер уменьшающий диапозон разброса при остановки стрельбы
+    private float curRazbrosTimeout;
     private float curReloadTimer;
     private int currentAmmo;
     private float curTimeout;
@@ -21,6 +29,9 @@ public class RayShooting : MonoBehaviour
         camera = Camera.main;
         currentAmmo = maxAmmo;
         curReloadTimer = reloadTimer;
+        curRazbrosTimeout = razbrosTimer;
+        thisRazbros = minRazbros;
+        curRazbros = razbros;
     }
 
     // Update is called once per frame
@@ -28,8 +39,20 @@ public class RayShooting : MonoBehaviour
     {
         if (Input.GetMouseButton(0) && curTimeout <= 0 && currentAmmo>0)
         {
+            curRazbros--;
+            curRazbrosTimeout = razbrosTimer;
+            float sdvigX = Random.Range(-thisRazbros, thisRazbros);//радномим отклонение
+            float sdvigY = Random.Range(-thisRazbros, thisRazbros);//рандомим отклонение
+            while((sdvigX* sdvigX+ sdvigY* sdvigY) > thisRazbros * thisRazbros)//проверяем точка в круге?
+            {
+                Debug.Log("Рандомим заново");//если нет
+                sdvigX = Random.Range(-thisRazbros, thisRazbros);//то рандомим заново
+                sdvigY = Random.Range(-thisRazbros, thisRazbros);
+            }
             currentAmmo--;
             Vector3 screenCneter = new Vector3(Screen.width / 2, Screen.height / 2, 0);//определяем центр камеры
+            screenCneter.x += sdvigX;//сдвигаем коорлинату
+            screenCneter.y += sdvigY;//сдвигаем координату
             Ray ray = camera.ScreenPointToRay(screenCneter);
             curTimeout = timeout;
             RaycastHit[] hit;
@@ -39,6 +62,7 @@ public class RayShooting : MonoBehaviour
                 if (i != targets)
                 {
                     Debug.Log("Damage:" + hit[i].collider.name);
+                    Debug.DrawLine(ray.origin, hit[i].point, Color.green, 5);
                 }
                 else
                 {
@@ -49,6 +73,15 @@ public class RayShooting : MonoBehaviour
         else
         {
             curTimeout -= Time.deltaTime;
+            curRazbrosTimeout -= Time.deltaTime;
+            if (curRazbrosTimeout <= 0)
+            {
+                thisRazbros -= step;
+                if (thisRazbros < minRazbros)
+                {
+                    thisRazbros = minRazbros;
+                }
+            }
         }
         if (Input.GetKey(KeyCode.R) && !isReload && !(currentAmmo==maxAmmo))
         {
@@ -56,13 +89,14 @@ public class RayShooting : MonoBehaviour
             isReload = true;
         }
         CheckAmmo();//функция отвечающая за перезарядку
+        CheckRazbros();//функция отвечающая за перезарядку
 
     }
     public void CheckAmmo() {
         
         if (currentAmmo <= 0 || isReload)//если патронов нету
         {Debug.Log("Reload:"+curReloadTimer);
-            if (curReloadTimer < 0)
+            if (curReloadTimer <= 0)
             {
                 isReload = false;
                 currentAmmo = maxAmmo;
@@ -71,6 +105,18 @@ public class RayShooting : MonoBehaviour
             else
             {
                 curReloadTimer -= Time.deltaTime;
+            }
+        }
+    }
+    public void CheckRazbros()
+    {
+        if (curRazbros<=0) 
+        {
+            curRazbros = razbros;
+            thisRazbros += step;
+            if (thisRazbros > maxRazbros)
+            {
+                thisRazbros = maxRazbros;
             }
         }
     }
